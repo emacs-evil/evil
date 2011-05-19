358b445880ad6622f2743e54e6bd34c773ad6077
Vegard Øye <vegard_oye@hotmail.com>
2011-05-20 00:16:56 +0200

    Add search motions
    Add "n", "N", "*", "#" and "gd". All highlight matches like
    "/" and "?" do, by calling the general function `evil-search'.

--- evil-search.el
+++ evil-search.el
@@ -3,8 +3,269 @@
 (require 'evil-vars)
 (require 'evil-common)
 
+(evil-define-motion evil-search-forward ()
+  (format "Search forward for user-entered text.
+Searches for regular expression if `evil-regexp-search' is t.%s"
+          (if (and (fboundp 'isearch-forward)
+                   (documentation 'isearch-forward))
+              (format "\n\nBelow is the documentation string \
+for `isearch-forward',\nwhich lists available keys:\n\n%s"
+                      (documentation 'isearch-forward)) ""))
+  :type exclusive
+  (evil-search-incrementally t evil-regexp-search))
 
+(evil-define-motion evil-search-backward ()
+  (format "Search backward for user-entered text.
+Searches for regular expression if `evil-regexp-search' is t.%s"
+          (if (and (fboundp 'isearch-forward)
+                   (documentation 'isearch-forward))
+              (format "\n\nBelow is the documentation string \
+for `isearch-forward',\nwhich lists available keys:\n\n%s"
+                      (documentation 'isearch-forward)) ""))
+  :type exclusive
+  (evil-search-incrementally nil evil-regexp-search))
 
+(evil-define-motion evil-search-next (count)
+  "Repeat the last search."
+  :type exclusive
+  (dotimes (var (or count 1))
+    (evil-search (if evil-regexp-search
+                     (car-safe regexp-search-ring)
+                   (car-safe search-ring))
+                 isearch-forward evil-regexp-search)))
+
+(evil-define-motion evil-search-previous (count)
+  "Repeat the last search in the opposite direction."
+  :type exclusive
+  (dotimes (var (or count 1))
+    (evil-search (if evil-regexp-search
+                     (car-safe regexp-search-ring)
+                   (car-safe search-ring))
+                 (not isearch-forward) evil-regexp-search)))
+
+(evil-define-motion evil-search-symbol-backward (count)
+  "Search backward for symbol under point."
+  :type exclusive
+  (dotimes (var (or count 1))
+    (evil-search-symbol nil)))
+
+(evil-define-motion evil-search-symbol-forward (count)
+  "Search forward for symbol under point."
+  :type exclusive
+  (dotimes (var (or count 1))
+    (evil-search-symbol t)))
+
+(evil-define-motion evil-goto-definition ()
+  "Go to definition or first occurrence of symbol under point."
+  :type exclusive
+  (let* ((string (or (thing-at-point 'symbol) ""))
+         (search (if evil-regexp-search (regexp-quote string) string))
+         ientry ipos)
+    ;; load imenu if available
+    (unless (featurep 'imenu)
+      (condition-case nil
+          (require 'imenu)
+        (error nil)))
+    (cond
+     ((string= string "")
+      (error "No string under cursor"))
+     ;; if imenu is available, try it
+     ((fboundp 'imenu--make-index-alist)
+      (condition-case nil
+          (setq ientry (imenu--make-index-alist))
+        (error nil))
+      (setq ientry (assoc string ientry))
+      (setq ipos (cdr ientry))
+      (unless (markerp ipos)
+        (setq ipos (cadr ientry)))
+      (cond
+       ;; imenu found a position, so go there and
+       ;; highlight the occurrence
+       ((and (markerp ipos)
+             (eq (marker-buffer ipos) (current-buffer)))
+        (evil-search search t evil-regexp-search ipos))
+       ;; imenu failed, so just go to first occurrence in buffer
+       (t
+        (evil-search search t evil-regexp-search (point-min)))))
+     ;; no imenu, so just go to first occurrence in buffer
+     (t
+      (evil-search search t evil-regexp-search (point-min))))))
+
+(defun evil-search-incrementally (forward regexp-p)
+  "Search incrementally for user-entered text."
+  (let ((evil-search-prompt (evil-search-prompt forward))
+        (isearch-search-fun-function 'evil-isearch-function)
+        (point (point))
+        search-nonincremental-instead)
+    (setq isearch-forward forward)
+    (evil-save-echo-area
+      (if forward
+          (isearch-forward regexp-p)
+        (isearch-backward regexp-p))
+      (when (and (eq point (point))
+                 (not (string= isearch-string "")))
+        (if forward
+            (isearch-repeat-forward)
+          (isearch-repeat-backward))
+        (isearch-exit))
+      ;; always position point at the beginning of the match
+      (when (and forward isearch-other-end)
+        (goto-char isearch-other-end))
+      (evil-flash-search-pattern
+       (evil-search-message isearch-string forward)))))
+
+(defun evil-flash-search-pattern (string &optional all)
+  "Flash last search matches for duration of `evil-flash-delay'.
+If ALL is non-nil, flash all matches. STRING is a string
+to display in the echo area."
+  (let ((lazy-highlight-initial-delay 0)
+        (isearch-search-fun-function 'evil-isearch-function)
+        (isearch-case-fold-search case-fold-search)
+        (disable (lambda (&optional arg) (evil-flash-hook t))))
+    (when evil-flash-timer
+      (cancel-timer evil-flash-timer))
+    (unless (or (null string)
+                (string= string ""))
+      (evil-echo-area-save)
+      (evil-echo string)
+      (isearch-highlight (match-beginning 0) (match-end 0))
+      (when all
+        (setq isearch-lazy-highlight-wrapped nil
+              isearch-lazy-highlight-start (point)
+              isearch-lazy-highlight-end (point))
+        (isearch-lazy-highlight-new-loop)
+        (unless isearch-lazy-highlight-overlays
+          (isearch-lazy-highlight-update)))
+      (add-hook 'pre-command-hook 'evil-flash-hook)
+      (setq evil-flash-timer
+            (run-at-time evil-flash-delay nil disable)))))
+
+(defun evil-flash-hook (&optional force)
+  "Disable hightlighting if `this-command' is not search.
+Disable anyway if FORCE is t."
+  (when (or force
+            ;; to avoid flicker, don't disable highlighting
+            ;; if the next command is also a search command
+            (not (memq this-command
+                       '(evil-search-backward
+                         evil-search-forward
+                         evil-search-next
+                         evil-search-previous
+                         evil-search-symbol-backward
+                         evil-search-symbol-forward))))
+    (evil-echo-area-restore)
+    (isearch-dehighlight)
+    (setq isearch-lazy-highlight-last-string nil)
+    (lazy-highlight-cleanup t)
+    (when evil-flash-timer
+      (cancel-timer evil-flash-timer)))
+  (remove-hook 'pre-command-hook 'evil-flash-hook))
+
+(defun evil-search-function (&optional forward regexp-p wrap)
+  "Return a search function.
+If FORWARD is nil, search backward, otherwise forward.
+If REGEXP-P is non-nil, the input is a regular expression.
+If WRAP is non-nil, the search wraps around the top or bottom
+of the buffer."
+  `(lambda (string &optional bound noerror count)
+     (let ((start (point))
+           (search-fun ',(if regexp-p
+                             (if forward
+                                 're-search-forward
+                               're-search-backward)
+                           (if forward
+                               'search-forward
+                             'search-backward)))
+           result)
+       (setq result (funcall search-fun string bound
+                             ,(if wrap t 'noerror) count))
+       (when (and ,wrap (null result))
+         (goto-char ,(if forward '(point-min) '(point-max)))
+         (unwind-protect
+             (setq result (funcall search-fun string bound noerror count))
+           (unless result
+             (goto-char start))))
+       result)))
+
+(defun evil-isearch-function ()
+  "Return a search function for use with isearch.
+Based on `isearch-regexp' and `isearch-forward'."
+  (evil-search-function isearch-forward evil-regexp-search evil-search-wrap))
+
+(defun evil-search (string forward &optional regexp-p start)
+  "Search for STRING and highlight matches.
+If FORWARD is nil, search backward, otherwise forward.
+If REGEXP-P is non-nil, STRING is taken to be a regular expression.
+START is the position to search from; if unspecified, it is
+one more than the current position."
+  (when (and (stringp string)
+             (not (string= string "")))
+    (let* ((orig (point))
+           (start (or start
+                      (if forward
+                          (min (point-max) (1+ orig))
+                        orig)))
+           (isearch-regexp regexp-p)
+           (isearch-forward forward)
+           (search-func (evil-search-function
+                         forward regexp-p evil-search-wrap)))
+      ;; no text properties, thank you very much
+      (set-text-properties 0 (length string) nil string)
+      ;; position to search from
+      (goto-char start)
+      (condition-case nil
+          (funcall search-func string)
+        (search-failed
+         (goto-char orig)
+         (error "\"%s\": %s not found"
+                string (if regexp-p "pattern" "string"))))
+      (setq isearch-string string)
+      (isearch-update-ring string regexp-p)
+      ;; always position point at the beginning of the match
+      (goto-char (match-beginning 0))
+      ;; determine message for echo area
+      (cond
+       ((and forward (< (point) start))
+        (setq string "Search wrapped around BOTTOM of buffer"))
+       ((and (not forward) (> (point) start))
+        (setq string "Search wrapped around TOP of buffer"))
+       (t
+        (setq string (evil-search-message string forward))))
+      (evil-flash-search-pattern string t))))
+
+(defun evil-search-symbol (forward)
+  "Search for symbol near point.
+If FORWARD is nil, search backward, otherwise forward."
+  (let ((string (car-safe regexp-search-ring))
+        (move (if forward 'forward-char 'backward-char))
+        (end (if forward 'eobp 'bobp)))
+    (setq isearch-forward forward)
+    (cond
+     ((and (memq last-command
+                 '(evil-search-symbol-forward
+                   evil-search-symbol-backward))
+           (stringp string)
+           (not (string= string "")))
+      (evil-search string forward evil-search-wrap))
+     (t
+      (save-excursion
+        (setq string (or (thing-at-point 'symbol) ""))
+        ;; if there's nothing under point, go forwards
+        ;; (or backwards) to find it
+        (while (and (null string)
+                    (not (funcall end)))
+          (funcall move)
+          (setq string (thing-at-point 'symbol)))
+        (setq string (format "\\_<%s\\_>" (regexp-quote string))))
+      (evil-search string forward evil-search-wrap)))))
+
+(defun evil-search-prompt (forward)
+  "Return the search prompt for the given direction."
+  (if forward "/" "?"))
+
+(defun evil-search-message (string forward)
+  "Prefix STRING with the search prompt."
+  (format "%s%s" (evil-search-prompt forward) string))
 
 (defadvice isearch-message-prefix (around evil activate)
   "Use `evil-search-prompt'."
@@ -20,109 +281,9 @@
     ad-do-it))
 
 (defadvice isearch-lazy-highlight-search (around evil activate)
+  "Never wrap the search in this context."
   (let (evil-search-wrap)
     ad-do-it))
-
-;; if `evil-search-wrap' is t, we want the search to wrap
-(defun evil-search-fun-function ()
-  "Return a wrapping search function.
-Based on `isearch-regexp' and `isearch-forward'."
-  `(lambda (regexp &optional bound noerror count)
-     (let ((point (point))
-           (search-fun (if isearch-regexp
-                           (if isearch-forward
-                               're-search-forward
-                             're-search-backward)
-                         (if isearch-forward
-                             'search-forward
-                           'search-backward)))
-           result)
-       (setq result (funcall search-fun regexp bound t count))
-       (when (and (not result) evil-search-wrap)
-         (goto-char (if isearch-forward (point-min) (point-max)))
-         (setq result (funcall search-fun regexp bound t count))
-         (unless result
-           (goto-char point)))
-       result)))
-
-(defun evil-search-backward (&optional forward)
-  "Search incrementally for user-entered text."
-  (interactive)
-  (evil-search-forward (not forward)))
-
-(defun evil-search-forward (&optional backward)
-  "Search incrementally for user-entered text."
-  (interactive)
-  (let ((evil-search-prompt "/")
-        (isearch-forward (not backward))
-        (isearch-search-fun-function 'evil-search-fun-function)
-        (point (point))
-        search-nonincremental-instead)
-    (evil-save-echo-area
-      (isearch-forward isearch-regexp)
-      (when (and (eq point (point))
-                 (not (string= isearch-string "")))
-        (isearch-repeat-forward)
-        (isearch-exit))
-      (when isearch-other-end
-        (goto-char isearch-other-end))
-      (unless (string= isearch-string "")
-        (evil-flash-search-pattern)))))
-
-(put 'evil-search-forward 'function-documentation
-     (format "Search forward for user-entered text.
-Searches for regular expression if `isearch-regexp' is t.
-
-%s" (if (and (fboundp 'isearch-forward)
-             (documentation 'isearch-forward))
-        (format "Below is the documentation string for `isearch-forward',
-which lists available keys:
-
-%s" (documentation 'isearch-forward)))))
-
-(defun evil-flash-search-pattern (&optional all)
-  "Flash search matches for duration of `evil-flash-delay'."
-  (let ((lazy-highlight-initial-delay 0)
-        (isearch-search-fun-function 'evil-search-fun-function)
-        (isearch-case-fold-search case-fold-search)
-        (disable (lambda (&optional arg) (evil-flash-hook t))))
-    (when evil-flash-timer
-      (cancel-timer evil-flash-timer))
-    (isearch-highlight (match-beginning 0) (match-end 0))
-    (when all
-      (setq isearch-lazy-highlight-wrapped nil
-            isearch-lazy-highlight-start (point)
-            isearch-lazy-highlight-end (point))
-      (isearch-lazy-highlight-new-loop)
-      (unless isearch-lazy-highlight-overlays
-        (isearch-lazy-highlight-update)))
-    (add-hook 'pre-command-hook 'evil-flash-hook)
-    (setq evil-flash-timer
-          (run-at-time evil-flash-delay nil disable))))
-
-(defun evil-flash-hook (&optional force)
-  "Disable hightlighting if `this-command' is not search.
-Disable anyway if FORCE is t."
-  (when (or force
-            ;; to avoid flicker, don't disable highlighting if the
-            ;; next command is also a search command
-            (not (memq this-command
-                       '(evil-exec-mapped-kbd-macro
-                         evil-search
-                         evil-search-backward
-                         isearch-forward
-                         evil-search-next
-                         evil-search-Next
-                         evil-search-backward
-                         isearch-forward
-                         evil-search-backward-for-symbol-at-point
-                         evil-search-forward-for-symbol-at-point))))
-    (isearch-dehighlight)
-    (setq isearch-lazy-highlight-last-string nil)
-    (lazy-highlight-cleanup t)
-    (when evil-flash-timer
-      (cancel-timer evil-flash-timer)))
-  (remove-hook 'pre-command-hook 'evil-flash-hook))
 
 (provide 'evil-search)
 
