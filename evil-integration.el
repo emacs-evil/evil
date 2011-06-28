aea798ceb45d0222ce57e98fac5ae91e6028fc7e
Vegard Øye <vegard_oye@hotmail.com>
2011-06-28 15:32:45 +0200

    Make Debugger mode come up in Emacs state
    In Normal state, "q" records a keyboard macro rather than quitting.

--- evil-integration.el
+++ evil-integration.el
@@ -13,6 +13,10 @@
 
 (add-to-list 'evil-emacs-state-modes 'Custom-mode)
 
+;;; Debugger
+
+(add-to-list 'evil-emacs-state-modes 'debugger-mode)
+
 ;;; ERT
 
 (add-to-list 'evil-emacs-state-modes 'ert-results-mode)
@@ -22,10 +26,14 @@
 (add-to-list 'evil-motion-state-modes 'undo-tree-visualizer-mode)
 
 (when (boundp 'undo-tree-visualizer-map)
-  (define-key undo-tree-visualizer-map [remap evil-backward-char] 'undo-tree-visualize-switch-branch-left)
-  (define-key undo-tree-visualizer-map [remap evil-forward-char] 'undo-tree-visualize-switch-branch-right)
-  (define-key undo-tree-visualizer-map [remap evil-next-line] 'undo-tree-visualize-redo)
-  (define-key undo-tree-visualizer-map [remap evil-previous-line] 'undo-tree-visualize-undo))
+  (define-key undo-tree-visualizer-map [remap evil-backward-char]
+    'undo-tree-visualize-switch-branch-left)
+  (define-key undo-tree-visualizer-map [remap evil-forward-char]
+    'undo-tree-visualize-switch-branch-right)
+  (define-key undo-tree-visualizer-map [remap evil-next-line]
+    'undo-tree-visualize-redo)
+  (define-key undo-tree-visualizer-map [remap evil-previous-line]
+    'undo-tree-visualize-undo))
 
 (provide 'evil-integration)
 
