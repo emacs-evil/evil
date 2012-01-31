f439e8110918bc80648ee4d87b281389a2239baa
Vegard Øye <vegard_oye@hotmail.com>
2012-01-31 14:23:50 +0100

    Update comment block in evil.el
    Add complete installation instructions. Also update the contact info:
    I am formally the "maintainer", but bug reports, suggestions and
    patches should go to the bug tracker. Hopefully this will take some
    load off the mailing list.

--- evil.el
+++ evil.el
@@ -1,15 +1,29 @@
 ;;; evil.el --- extensible vi layer
 
-;; Author: Frank Fischer <frank.fischer at mathematik.tu-chemnitz.de>
-;;      Vegard Øye <vegard_oye at hotmail.com>
+;; Author: Vegard Øye <vegard_oye at hotmail.com>
+;;      Frank Fischer <frank.fischer at mathematik.tu-chemnitz.de>
 ;;      Nikolai Weibull <now at bitwi.se>
-;; Maintainer: <implementations-list at lists.ourproject.org>
+;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
+;;      To get in touch, please use the bug tracker or the
+;;      mailing list (see below).
 ;; Created: 2011-03-01
 ;; Version: 0.1
 ;; Keywords: emulation, vim
 ;; URL: http://gitorious.org/evil
+;;      Repository: git://gitorious.org/evil/evil.git
+;;      EmacsWiki: http://www.emacswiki.org/emacs/Evil
+;; Bug tracker: https://bitbucket.org/lyro/evil/issues
+;;      If you have bug reports, suggestions or patches, please
+;;      create an issue at the bug tracker (open for everyone).
+;;      Other discussions (tips, extensions) go to the mailing list.
+;; Mailing list: <implementations-list at lists.ourproject.org>
+;;      Subscribe: http://tinyurl.com/implementations-list
+;;      Newsgroup: nntp://news.gmane.org/gmane.emacs.vim-emulation
+;;      Archives: http://dir.gmane.org/gmane.emacs.vim-emulation
+;;      You don't have to subscribe to post; we usually reply
+;;      within a few days and CC our replies back to you.
 ;;
-;; This file is not part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
 ;;; Commentary:
 
@@ -21,25 +35,30 @@
 ;;
 ;;      git clone git://gitorious.org/evil/evil.git
 ;;
-;; Evil is discussed at <implementations-list at lists.ourproject.org>,
-;; a mailing list you can subscribe to at:
+;; Move Evil to ~/.emacs.d/evil (or somewhere else in the `load-path').
+;; Then add the following lines to ~/.emacs:
 ;;
-;; http://lists.ourproject.org/cgi-bin/mailman/listinfo/implementations-list
+;;      (add-to-list 'load-path "~/.emacs.d/evil")
+;;      (require 'evil)
+;;      (evil-mode 1)
 ;;
-;; Subscription is not required; we usually reply within a few days
-;; and CC our replies back to you.
+;; Evil requires undo-tree.el for linear undo and undo branches:
+;;
+;;      http://www.emacswiki.org/emacs/UndoTree
+;;
+;; Otherwise, Evil uses regular Emacs undo.
 
 ;;; License:
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of the
-;; License, or any later version.
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or any later version.
 ;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-;; General Public License for more details.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; if not, write to the Free Software
