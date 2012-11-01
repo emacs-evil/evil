1f08e7bb43147b4c8264fe47a5afcab2888091f8
Frank Fischer <frank.fischer@mathematik.tu-chemnitz.de>
2012-11-01 16:41:49 +0100

    Backout e40ba6482a86
    This addresses #189.

--- evil-vars.el
+++ evil-vars.el
@@ -559,7 +559,6 @@ If STATE is nil, Evil is disabled in the buffer."
     command-history-mode
     compilation-mode
     dictionary-mode
-    dired-mode
     ert-results-mode
     help-mode
     Info-mode
