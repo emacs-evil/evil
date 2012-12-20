7c4067d186091ed4a2516a30dab4ded1b7bf6aab
Frank Fischer <frank.fischer@mathematik.tu-chemnitz.de>
2012-12-20 10:25:43 +0100

    Fix alias `input-method-deactivate-hook' of `input-method-inactivate-hook'.
    This is a variable alias, not a function alias.
    
    This addresses #237.

--- evil-common.el
+++ evil-common.el
@@ -32,7 +32,7 @@
 (unless (fboundp 'deactivate-input-method)
   (defalias 'deactivate-input-method 'inactivate-input-method))
 (unless (boundp 'input-method-deactivate-hook)
-  (defalias 'input-method-deactivate-hook 'input-method-inactivate-hook))
+  (defvaralias 'input-method-deactivate-hook 'input-method-inactivate-hook))
 
 (condition-case nil
     (require 'windmove)
