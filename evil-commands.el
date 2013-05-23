d46131600aab2084372baeb9a61fc33f9dc0e522
Frank Fischer <frank.fischer@mathematik.tu-chemnitz.de>
2013-05-23 20:17:41 +0200

    Ensure `evil-delete-backward-word` only deletes newline at bol
    If point is not at the beginning of the line, the this function should
    only delete characters on the current line, even if they are only
    whitespaces. This corresponds to Vim's default behaviour.

--- evil-commands.el
+++ evil-commands.el
@@ -1212,9 +1212,11 @@ Save in REGISTER or in the kill-ring with YANK-HANDLER."
   "Delete previous word."
   (if (and (bolp) (not (bobp)))
       (delete-char -1)
-    (evil-delete (save-excursion
-                   (evil-backward-word-begin)
-                   (point))
+    (evil-delete (max
+                  (save-excursion
+                    (evil-backward-word-begin)
+                    (point))
+                  (line-beginning-position))
                  (point)
                  'exclusive
                  nil)))
