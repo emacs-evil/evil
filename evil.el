b48aa605e877702961fed7f640f669a6d6837eae
Frank Fischer <frank.fischer@mathematik.tu-chemnitz.de>
2013-06-07 05:35:49 +0200

    Bump version to 1.0.4

--- evil.el
+++ evil.el
@@ -24,7 +24,7 @@
 ;;      To get in touch, please use the bug tracker or the
 ;;      mailing list (see below).
 ;; Created: 2011-03-01
-;; Version: 1.0.3
+;; Version: 1.0.4
 ;; Keywords: emulation, vim
 ;; URL: http://gitorious.org/evil
 ;;      Repository: git://gitorious.org/evil/evil.git
