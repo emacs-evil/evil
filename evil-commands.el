f69e7a4506749d5505c848bda0002bd27f847d3f
Frank Fischer <frank.fischer@mathematik.tu-chemnitz.de>
2013-08-02 09:42:38 +0200

    Bump version to 1.0.7

--- evil-commands.el
+++ evil-commands.el
@@ -2,7 +2,7 @@
 ;; Author: Vegard Øye <vegard_oye at hotmail.com>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.0.6
+;; Version: 1.0.7
 
 ;;
 ;; This file is NOT part of GNU Emacs.
--- evil-pkg.el
+++ evil-pkg.el
@@ -1,5 +1,5 @@
 (define-package
   "evil"
-  "1.0.6"
+  "1.0.7"
   "Extensible Vi layer for Emacs."
   '((undo-tree "0.5.1")))
