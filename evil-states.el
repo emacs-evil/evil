4ecf042d7a324558cd505ee058fc198801100857
Frank Fischer <frank-fischer@shadow-soft.de>
2015-04-08 23:17:36 +0200

    update version to 1.1.0

--- evil-states.el
+++ evil-states.el
@@ -3,7 +3,7 @@
 ;; Author: Vegard Øye <vegard_oye at hotmail.com>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.1.1
+;; Version: 1.1.0
 
 ;;
 ;; This file is NOT part of GNU Emacs.
