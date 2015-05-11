aae92ec452377159606877bdb3338242d2fbab78
Frank Fischer <frank-fischer@shadow-soft.de>
2015-05-11 10:46:16 +0200

    update version to 1.1.6

--- evil-command-window.el
+++ evil-command-window.el
@@ -2,7 +2,7 @@
 ;; Author: Emanuel Evans <emanuel.evans at gmail.com>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.1.5
+;; Version: 1.1.6
 
 ;;
 ;; This file is NOT part of GNU Emacs.
