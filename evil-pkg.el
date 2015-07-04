10c781e3f995dad7422ecd3f3f061c3a026818bb
Frank Fischer <frank-fischer@shadow-soft.de>
2015-07-04 11:52:04 +0200

    update version to 1.2.0

--- evil-pkg.el
+++ evil-pkg.el
@@ -1,6 +1,6 @@
 (define-package
   "evil"
-  "1.1.6"
+  "1.2.0"
   "Extensible Vi layer for Emacs."
   '((undo-tree "0.6.3")
     (goto-chg "1.6")))
--- evil-vars.el
+++ evil-vars.el
@@ -3,7 +3,7 @@
 ;; Author: Vegard Øye <vegard_oye at hotmail.com>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.1.6
+;; Version: 1.2.0
 
 ;;
 ;; This file is NOT part of GNU Emacs.
@@ -1705,7 +1705,7 @@ Otherwise the previous command is assumed as substitute.")
           (goto-char (point-min))
           (buffer-substring (point-min) (line-end-position)))
          ;; no repo, use plain version
-         (t "1.1.6")))))
+         (t "1.2.0")))))
   "The current version of Evil")
 
 (defun evil-version ()
