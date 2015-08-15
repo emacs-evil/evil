11309f4051b2ab04545dc0a00f1612bc249f1320
Frank Fischer <frank-fischer@shadow-soft.de>
2015-08-16 01:55:29 +0200

    update version to 1.2.3

--- evil-types.el
+++ evil-types.el
@@ -3,7 +3,7 @@
 ;; Author: Vegard Øye <vegard_oye at hotmail.com>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.2.2
+;; Version: 1.2.3
 
 ;;
 ;; This file is NOT part of GNU Emacs.
