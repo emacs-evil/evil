5665e1508467e8b9eb32ac469c55a4b7642283b1
Frank Fischer <frank-fischer@shadow-soft.de>
2015-11-09 17:29:20 +0100

    search: show default search string in shadow face (fix #591)

--- evil-search.el
+++ evil-search.el
@@ -1045,6 +1045,10 @@ current search result."
             (goto-char (+ beg count))
             (setq evil-this-type 'inclusive))))))))
 
+(defun evil-ex-search-setup ()
+  "Hook to initialize the minibuffer for ex search."
+  (add-hook 'pre-command-hook #'evil-ex-remove-default))
+
 (defun evil-ex-start-search (direction count)
   "Start a new search in a certain DIRECTION."
   ;; store buffer and window where the search started
@@ -1060,9 +1064,15 @@ current search result."
       (let* ((minibuffer-local-map evil-ex-search-keymap)
              (search-string
               (condition-case err
-                  (read-string (if (eq evil-ex-search-direction 'forward)
-                                   "/" "?")
-                               nil 'evil-ex-search-history)
+                  (minibuffer-with-setup-hook
+                      #'evil-ex-search-setup
+                    (read-string (if (eq evil-ex-search-direction 'forward)
+                                     "/" "?")
+                                 (and evil-ex-search-history
+                                      (propertize
+                                       (car evil-ex-search-history)
+                                       'face 'shadow))
+                                 'evil-ex-search-history))
                 (quit
                  (evil-ex-search-stop-session)
                  (evil-ex-delete-hl 'evil-ex-search)
