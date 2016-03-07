6a114be777786b05997777c1b6bbf67eccf0ce12
Frank Fischer <frank-fischer@shadow-soft.de>
2016-03-07 08:10:01 +0100

    update version to 1.2.11

--- evil-macros.el
+++ evil-macros.el
@@ -3,7 +3,7 @@
 ;; Author: Vegard Øye <vegard_oye at hotmail.com>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.2.10
+;; Version: 1.2.11
 
 ;;
 ;; This file is NOT part of GNU Emacs.
--- evil-repeat.el
+++ evil-repeat.el
@@ -3,7 +3,7 @@
 ;; Author: Frank Fischer <frank.fischer at mathematik.tu-chemnitz.de>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.2.10
+;; Version: 1.2.11
 
 ;;
 ;; This file is NOT part of GNU Emacs.
