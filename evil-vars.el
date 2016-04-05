0ad4c2dae1249558f7b59a78a685e4f8092009c9
Frank Fischer <frank-fischer@shadow-soft.de>
2016-04-05 07:17:10 +0200

    update version to 1.2.12

--- evil-vars.el
+++ evil-vars.el
@@ -3,7 +3,7 @@
 ;; Author: Vegard Øye <vegard_oye at hotmail.com>
 ;; Maintainer: Vegard Øye <vegard_oye at hotmail.com>
 
-;; Version: 1.2.11
+;; Version: 1.2.12
 
 ;;
 ;; This file is NOT part of GNU Emacs.
@@ -1712,7 +1712,7 @@ Otherwise the previous command is assumed as substitute.")
           (goto-char (point-min))
           (buffer-substring (point-min) (line-end-position)))
          ;; no repo, use plain version
-         (t "1.2.11")))))
+         (t "1.2.12")))))
   "The current version of Evil")
 
 (defun evil-version ()
--- evil.el
+++ evil.el
@@ -45,7 +45,7 @@
 ;;      To get in touch, please use the bug tracker or the
 ;;      mailing list (see below).
 ;; Created: 2011-03-01
-;; Version: 1.2.11
+;; Version: 1.2.12
 ;; Keywords: emulation, vim
 ;; URL: http://gitorious.org/evil
 ;;      Repository: git://gitorious.org/evil/evil.git
