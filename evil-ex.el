2a6e58d72ca2051889b5f4887f85006387669178
Alexander Shukaev <projects@Alexander.Shukaev.name>
2019-01-25 02:26:24 +0100

    Allow colon `:` for symbols in `binding` of `evil-ex-grammar`

--- evil-ex.el
+++ evil-ex.el
@@ -56,7 +56,7 @@
      number)
     (command #'evil-ex-parse-command)
     (binding
-     "[~&*@<>=:]+\\|[[:alpha:]_]+\\|!")
+     "[~&*@<>=:]+\\|[[:alpha:]_:]+\\|!")
     (emacs-binding
      "[[:alpha:]-][[:alnum:][:punct:]-]+")
     (bang
