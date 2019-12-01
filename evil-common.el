d616c03dcf9967dbe12fe6dfea06bc94aa752fa9
Sanel Zukan <karijes@users.sourceforge.net>
2019-12-01 12:39:40 +0100

    Use naming evil naming schema.

--- evil-common.el
+++ evil-common.el
@@ -2150,7 +2150,7 @@ to keep Vim compatibility with register jumps."
                             (cons reg (evil-get-register reg t)))
                         '(?\" ?* ?+ ?% ?# ?/ ?: ?. ?-
                               ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9))
-                (cl-remove-if-not (lambda (x) (number-or-marker-p x)) register-alist)
+                (cl-remove-if-not #'(lambda (reg) (number-or-marker-p reg)) register-alist)
                 nil)
         #'(lambda (reg1 reg2) (< (car reg1) (car reg2)))))
 
