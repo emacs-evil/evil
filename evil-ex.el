f009995ba1494581ff44831086e0a8d190ca24ed
codygman <cody@codygman.dev>
2021-01-08 14:49:07 -0600

    pass in some value for required when argument

--- evil-ex.el
+++ evil-ex.el
@@ -595,7 +595,8 @@ argument handler that requires shell completion."
 
 (define-obsolete-function-alias
   'evil-ex-shell-command-completion-at-point
-  'comint-completion-at-point)
+  'comint-completion-at-point
+  "22.1")
 
 (evil-ex-define-argument-type shell
   "Shell argument type, supports completion."
