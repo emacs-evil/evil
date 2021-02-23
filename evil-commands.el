6dd031dc3fd6eea75c3930e6036d2fd4cf70da1e
Tom Dalziel <tom_dl@hotmail.com>
2021-02-23 23:05:32 +0000

    Refactor evil-delete-marks code

--- evil-commands.el
+++ evil-commands.el
@@ -3340,6 +3340,22 @@ corresponding to the characters of this string are shown."
   (switch-to-buffer (car (elt entry 3)))
   (evil-goto-mark (string-to-char (elt entry 0))))
 
+(defun evil--parse-delmarks (to-be-parsed &optional parsed)
+  "Where TO-BE-PARSED can contain ranges in the form `x-y'.
+PARSED is a list of characters whose marks should be deleted."
+  (let ((a (car to-be-parsed))
+        (b (or (nth 1 to-be-parsed) ?\s)) ; handle `-'
+        (c (or (nth 2 to-be-parsed) ?\s))) ; handle `x-'
+    (cond
+     ((eq ?- a) (user-error "Invalid argument: %s" (string a b)))
+     ((eq ?- b) (if (or (<= ?a a c ?z) (<= ?A a c ?Z) (<= ?0 a c ?9))
+                    (evil--parse-delmarks (nthcdr 3 to-be-parsed)
+                                          (append parsed (number-sequence a c)))
+                  (user-error "Invalid argument: %s" (string a b c))))
+     ((eq ?- c) (evil--parse-delmarks (cdr to-be-parsed) (push a parsed)))
+     ((>= 3 (length to-be-parsed)) (append parsed to-be-parsed))
+     (t (evil--parse-delmarks (nthcdr 2 to-be-parsed) (append parsed (list a b)))))))
+
 (evil-define-command evil-delete-marks (marks &optional force)
   "Delete all marks.
 MARKS is a string denoting all marks to be deleted. Mark names are
@@ -3348,48 +3364,20 @@ either single characters or a range of characters in the form A-Z.
 If FORCE is non-nil all local marks except 0-9 are removed.
 "
   (interactive "<a><!>")
-  (cond
-   ;; delete local marks except 0-9
-   (force
-    (setq evil-markers-alist
-          (cl-delete-if (lambda (m)
-                          (not (and (>= (car m) ?0) (<= (car m) ?9))))
-                        evil-markers-alist)))
-   (t
-    (let ((i 0)
-          (n (length marks))
-          delmarks)
-      (while (< i n)
-        (cond
-         ;; skip spaces
-         ((= (aref marks i) ?\s) (cl-incf i))
-         ;; ranges of marks
-         ((and (< (+ i 2) n)
-               (= (aref marks (1+ i)) ?-)
-               (or (and (>= (aref marks i) ?a)
-                        (<= (aref marks i) ?z)
-                        (>= (aref marks (+ 2 i)) ?a)
-                        (<= (aref marks (+ 2 i)) ?z))
-                   (and (>= (aref marks i) ?A)
-                        (<= (aref marks i) ?Z)
-                        (>= (aref marks (+ 2 i)) ?A)
-                        (<= (aref marks (+ 2 i)) ?Z))))
-          (let ((m (aref marks i)))
-            (while (<= m (aref marks (+ 2 i)))
-              (push m delmarks)
-              (cl-incf m)))
-          (cl-incf i 2))
-         ;; single marks
-         (t
-          (push (aref marks i) delmarks)
-          (cl-incf i))))
-      ;; now remove all marks
+  (if force
+      ;; delete local marks except 0-9
+      (setq evil-markers-alist
+            (cl-delete-if (lambda (m) (not (<= ?0 (car m) ?9)))
+                          evil-markers-alist))
+    (let ((delmarks (evil--parse-delmarks (remove ?\s (append marks nil)))))
+      ;; delete all parsed marks...
       (setq evil-markers-alist
             (cl-delete-if (lambda (m) (member (car m) delmarks))
                           evil-markers-alist))
+      ;; ensure all parsed marks are deleted globally...
       (set-default 'evil-markers-alist
                    (cl-delete-if (lambda (m) (member (car m) delmarks))
-                                 (default-value 'evil-markers-alist)))))))
+                                 (default-value 'evil-markers-alist))))))
 
 (eval-when-compile (require 'ffap))
 (evil-define-command evil-find-file-at-point-with-line ()
