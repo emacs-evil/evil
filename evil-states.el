50a03220ed3e4d7bd1630d716556321b5482c599
Tom Dalziel <tom_dl@hotmail.com>
2021-11-15 23:09:27 +0000

    Refactor `evil-visual-update-x-selection`, remove docstring
    Docstring should be generated by sphinx, not manually

--- evil-states.el
+++ evil-states.el
@@ -365,16 +365,16 @@ otherwise exit Visual state."
 
 (defun evil-visual-update-x-selection (&optional buffer)
   "Update the X selection with the current visual region of BUFFER."
-  (when evil-visual-update-x-selection-p
-    (let ((buf (or buffer (current-buffer))))
-      (when (buffer-live-p buf)
-        (with-current-buffer buf
-          (when (and (evil-visual-state-p)
-                     (display-selections-p)
-                     (not (eq evil-visual-selection 'block)))
-            (evil-set-selection 'PRIMARY (buffer-substring-no-properties
-                                          evil-visual-beginning
-                                          evil-visual-end))))))))
+  (let ((buf (or buffer (current-buffer))))
+    (when (and evil-visual-update-x-selection-p
+               (buffer-live-p buf)
+               (evil-visual-state-p)
+               (display-selections-p)
+               (not (eq evil-visual-selection 'block)))
+      (with-current-buffer buf
+        (evil-set-selection 'PRIMARY (buffer-substring-no-properties
+                                      evil-visual-beginning
+                                      evil-visual-end))))))
 
 (defun evil-visual-activate-hook (&optional _command)
   "Enable Visual state if the region is activated."
