cddc4a67579daafcf718afd8b757b95d77e01198
Tom Dalziel <tom_dl@hotmail.com>
2022-10-13 12:14:40 +0100

    Shorten doc-strings

--- evil-vars.el
+++ evil-vars.el
@@ -2116,7 +2116,7 @@ to `undo-redo', Evil uses commands natively available in Emacs 28."
            (set-default sym value)))
 
 (defcustom evil-visual-update-x-selection-p t
-  "Whether to update the X PRIMARY selection with the current visual region automatically."
+  "Whether to update the X PRIMARY selection with the current visual region."
   :type  'boolean
   :group 'evil)
 
