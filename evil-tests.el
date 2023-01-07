emacs-evil/evil

commit e4fb2a37c5bd3723c4869d7b6728a43b7321876e

Return to state after C-o in macro (#1549)

evil-commands.el
@@ -2358,7 +2358,10 @@ when called interactively."
    (t
     (condition-case err
         (evil-with-single-undo
-          (execute-kbd-macro macro count))
+          (dotimes (_ (or count 1))
+            (execute-kbd-macro macro)
+            (when (eq 'evil-execute-in-normal-state last-command)
+              (evil-change-state evil--execute-normal-return-state))))
       ;; enter Normal state if the macro fails
       (error
        (evil-normal-state)

evil-tests.el
@@ -356,7 +356,13 @@ with `M-x evil-tests-run'"))
         :state insert
         "ab[]cfg"
         ("\C-o~de\C-o.")
-        "abCdeF[]g"))))
+        "abCdeF[]g"))
+    (ert-info ("Returned to initial state when finishing a macro")
+      (evil-test-buffer
+        "[f]oo __ bar __ baz __ qux"
+        (evil-set-register ?q "f_cehi\C-o")
+        ("@q1" [escape] "@q2")
+        "foo hi1 bar hi2[] baz __ qux"))))
 
 (defun evil-test-suppress-keymap (state)
   "Verify that `self-insert-command' is suppressed in STATE"

commit 53bd66fcc7e61f6a1890c5b113cafae10a243fcc

Fix executing macro infinite times
This commit fixes a regression caused by #1549 where you could no longer
give a prefix argument to execute the macro in an infinite loop.

evil-commands.el
@@ -2460,7 +2460,7 @@ will be opened instead."
       (evil-set-register evil-this-macro nil)
       (kmacro-start-macro nil)
       (setq evil-macro-buffer (current-buffer)))
-     (t (error "Invalid register")))))
+     (t (error "Invalid register `%s'" register)))))
 
 (evil-define-command evil-execute-macro (count macro)
   "Execute keyboard macro MACRO, COUNT times.
@@ -2472,15 +2472,14 @@ when called interactively."
   :suppress-operator t
   (interactive
    (let (count macro register)
-     (setq count (if current-prefix-arg
-                     (if (numberp current-prefix-arg)
-                         current-prefix-arg
-                       0) 1)
+     (setq count (cond ((null current-prefix-arg) 1)
+                       ((numberp current-prefix-arg) current-prefix-arg)
+                       (t 0))
            register (or evil-this-register (read-char)))
      (cond
-      ((or (and (eq register ?@) (eq evil-last-register ?:))
-           (eq register ?:))
-       (setq macro (lambda () (evil-ex-repeat nil))
+      ((or (eq register ?:)
+           (and (eq register ?@) (eq evil-last-register ?:)))
+       (setq macro #'evil-ex-repeat
              evil-last-register ?:))
       ((eq register ?@)
        (unless evil-last-register
@@ -2493,24 +2492,22 @@ when called interactively."
   (cond
    ((functionp macro)
     (evil-repeat-abort)
-    (dotimes (_ (or count 1))
-      (funcall macro)))
+    (if (zerop count)
+        (while t (funcall macro))
+      (dotimes (_ (or count 1)) (funcall macro))))
    ((or (and (not (stringp macro))
              (not (vectorp macro)))
         (member macro '("" [])))
     ;; allow references to currently empty registers
     ;; when defining macro
-    (unless evil-this-macro
-      (user-error "No previous macro")))
+    (unless evil-this-macro (user-error "No previous macro")))
    (t
     (condition-case err
         (evil-with-single-undo
-          (dotimes (_ (or count 1))
-            (execute-kbd-macro macro)))
+          (execute-kbd-macro macro count))
       ;; enter Normal state if the macro fails
       (error
        (evil-normal-state)
-       (evil-normalize-keymaps)
        (signal (car err) (cdr err)))))))
 
 (evil-define-command evil-execute-last-recorded-macro (count)

evil-ex.el
@@ -675,8 +675,8 @@ This function interprets special file names like # and %."
   (unless (zerop (length evil-ex-argument))
     (evil-ex-replace-special-filenames evil-ex-argument)))
 
-(defun evil-ex-repeat (count)
-  "Repeat the last ex command."
+(defun evil-ex-repeat (&optional count)
+  "Repeat the last Ex command."
   (interactive "P")
   (when count
     (goto-char (point-min))

evil-tests.el
@@ -9567,6 +9567,15 @@ Source
       ("ci|testing" [escape])
       "| foo |testing| bar |")))
 
+(ert-deftest evil-test-kbd-macro ()
+  "Test recording and replaying of macros."
+  :tags '(evil kbd-macro)
+  (ert-info ("Execute macro an infinite number of times")
+    (evil-test-buffer "xxx"
+      (evil-set-register ?q "ryl")
+      (error 'end-of-line "\C-u@q")
+      "yyy")))
+
 (ert-deftest evil-test-undo-kbd-macro ()
   "Test if evil can undo the changes made by a keyboard macro
 when an error stops the execution of the macro"