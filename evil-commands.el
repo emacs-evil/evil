953cf213dca27af9041d0176e1ef321fe1d6f192
Axel Forsman <axelsfor@gmail.com>
2023-04-23 16:49:45 +0200

    Generate docstringdb.json at build time
    Closes #1544. Note that the generated Texinfo file cannot be removed as
    it is built by Sphinx which is not available during ELPA package builds.

--- evil-commands.el
+++ evil-commands.el
@@ -824,16 +824,17 @@ Columns are counted from zero."
   (evil-goto-mark char noerror)
   (evil-first-non-blank))
 
+(declare-function goto-last-change "ext:goto-chg")
+(declare-function goto-last-change-reverse "ext:goto-chg")
+
 (evil-define-motion evil-goto-last-change (count)
   "Like `goto-last-change' but takes a COUNT rather than a span."
-  (eval-and-compile (require 'goto-chg))
   (setq this-command 'goto-last-change)
   (dotimes (_ (or count 1))
     (goto-last-change nil)))
 
 (evil-define-motion evil-goto-last-change-reverse (count)
   "Like `goto-last-change-reverse' but takes a COUNT rather than a span."
-  (eval-and-compile (require 'goto-chg))
   (setq this-command 'goto-last-change-reverse)
   (dotimes (_ (or count 1))
     (goto-last-change-reverse nil)))
