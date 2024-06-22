a837719da7549ada2ed1a19520f7eb1e28f3ca96
liuzhishan01 <liuzhishan01@gmail.com>
2024-06-22 16:32:02 +0800

    fix some logic in evil-open-file-under-cursor

--- evil-commands.el
+++ evil-commands.el
@@ -3727,28 +3727,25 @@ Supports positions in the following formats: \"path:line path(line)\",
         (when column-number
           (move-to-column (1- column-number)))))))
 
-(evil-define-command evil-open-file-under-cursor ()
+(evil-define-command evil-open-file-under-cursor (file-path)
   "Open the file under cursor and go to position if present.
 Supports both relative path and absolute path. If path is relative, then find common
 ancestor of path and current buffer file. If path is absolute path, go to the file
-directly. Otherwise, fallback to evil-find-file-at-point-with-line."
-  (interactive)
-  (let* ((file-path (thing-at-point 'filename t)))
-    (if (file-name-absolute-p file-path)
-    (find-file file-path)
-      (let* ((parts (split-string (buffer-file-name) "/"))
-        (target-filename "")
-        (target-exists nil))
+directly. Otherwise, fallback to `evil-find-file-at-point-with-line`."
+  (interactive
+   (let ((file-path (thing-at-point 'filename t)))
+     (list file-path)))
 
-        (dolist (n (number-sequence 1 (length parts)) target-filename)
-          (let* ((new-filename (string-join (append (-take n parts) (list file-path)) "/")))
-            (if (file-exists-p new-filename)
-                (setq target-filename new-filename
-                    target-exists t))))
+  (if (file-name-absolute-p file-path)
+      (find-file file-path)
 
-        (if target-exists
-            (find-file target-filename)
-          (evil-find-file-at-point-with-line))))))
+    (if (and (stringp file-path)
+             (string-match-p "/" file-path))
+        (let ((dirname (locate-dominating-file default-directory file-path)))
+          (if (file-in-directory-p dirname (doom-project-root))
+              (find-file (doom-path dirname file-path))
+            (evil-find-file-at-point-with-line)))
+      (evil-find-file-at-point-with-line))))
 
 (evil-define-command evil-find-file-at-point-visual ()
   "Find the filename selected by the visual region.
